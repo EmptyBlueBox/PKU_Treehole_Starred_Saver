b58b869067fad733c30065c8f55260fcda3bc3af
Yutong Liang <lyt0112@outlook.com>
2025-07-24 15:49:44 +0800

    feat: add configuration options for request limits in config.py

--- config.py
+++ config.py
@@ -4,8 +4,12 @@ Configuration file for user credentials.
 Attributes:
     USERNAME (str): The username for login authentication.
     PASSWORD (str): The password for login authentication.
+    MAX_SUBMITTED_REQUESTS_PER_SECOND (int): The maximum number of requests per second.
+    MAX_PARALLEL_REQUESTS (int): The maximum number of parallel requests.
 """
 
 # Set your username and password here
 USERNAME = "2xx00xxxxx"  # type: str
 PASSWORD = "xxxxxx"  # type: str
+MAX_SUBMITTED_REQUESTS_PER_SECOND = 20  # type: int
+MAX_PARALLEL_REQUESTS = 10  # type: int
